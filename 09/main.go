9c2225e57c82422af626ec3596b2f2e63e2c1568
yury6472 <YCheckotovsky@gmail.com>
2020-09-25 09:46:14 +0300

    changed port

--- 09/main.go
+++ 09/main.go
@@ -9,6 +9,6 @@ func main() {
 	http.HandleFunc("/employee", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Привет Интернет")
 	})
-
+	// Use port 9090
 	http.ListenAndServe(":9090", nil)
 }
